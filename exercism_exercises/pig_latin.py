45e60c3f7817e6f057d1cdec07e4194b3b65a2a0
Michal Moudrý <moudry.michal@live.com>
2022-09-28 13:03:25 +0200

    Started pig lating exercise

---
+++ exercism_exercises/pig_latin.py
@@ -0,0 +1,12 @@
+"""
+Pig latin exercise module.
+"""
+
+VOWELS = ["a", "e", "i", "o", "u"]
+CONSONANTS = ["b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p", "q", "s", "t", "v", "x", "z", "h", "r", "w", "y"]
+CONSONANT_CLUSTERS = []
+
+def translate(text: str) -> str:
+    return text
+
+print(translate("Pig"))
