19c2158475008ae5e1b597a7d8e8901bd66e26aa
Michal Moudrý <moudry.michal@live.com>
2022-10-15 11:56:51 +0200

    Finished darts exercise

--- exercism_exercises/darts.py
+++ exercism_exercises/darts.py
@@ -3,9 +3,14 @@ Darts exercise module.
 """
 
 def score(x: float, y: float) -> int:
-    if x < 0: x = -x
-    if y < 0: y = -y
-    print(x, y)
-    return 10
+    x2, y2 = pow(x, 2), pow(y, 2)
+    a2 = x2 + y2
+    if a2 <= 1:
+        return 10
+    if a2 <= 25:
+        return 5
+    if a2 <= 100:
+        return 1
+    return 0
 
-print(score(0.8, -0.8))
+print(score(-9, 9), "=> Expected:", 0)
