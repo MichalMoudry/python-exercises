fb4606dff484914c82704c34a668e656e932ca31
Michal Moudrý <moudry.michal@live.com>
2022-10-22 11:55:19 +0200

    Finished isogram exercise

--- exercism_exercises/isogram.py
+++ exercism_exercises/isogram.py
@@ -3,4 +3,12 @@ Isogram exercise module.
 """
 
 def is_isogram(string: str) -> bool:
-    pass
+    string = string.lower().replace("-", "").replace(" ", "")
+    for char in enumerate(string):
+        if char[0] != string.index(char[1]):
+            return False
+    return True
+
+print(is_isogram("Alphabet"), "- Expected: False")
+print(is_isogram("abc-def-"), "- Expected: True")
+print(is_isogram("isograms"), "- Expected: False")
