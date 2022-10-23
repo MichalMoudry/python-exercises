6ee0a2f82b370f4aab56a18021917d165b56ee45
Michal Moudrý <moudry.michal@live.com>
2022-10-23 12:53:05 +0200

    Updated sublist exercise

--- exercism_exercises/sublist.py
+++ exercism_exercises/sublist.py
@@ -8,18 +8,31 @@ SUPERLIST = "superlist"
 EQUAL = "equal"
 UNEQUAL = "unequal"
 
+def eval_superlist(list_one: list, list_two: list) -> str:
+    for item in enumerate(list_two):
+        if item[1] != list_one[item[0]]:
+            return UNEQUAL
+    return SUPERLIST
+
+def eval_sublist(list_one: list, list_two: list) -> str:
+    return SUBLIST
+
 def sublist(list_one: list, list_two: list) -> str:
     if list_one == list_two:
         return EQUAL
     try:
-        test = [list_two.index(item) for item in list_one]
-        print(test)
-    except ValueError:
+        result = eval_superlist(list_one, list_two)
+        if result == SUPERLIST:
+            return SUPERLIST
+    except:
+        result = eval_sublist(list_one, list_two)
+        if result == SUBLIST:
+            return SUBLIST
         return UNEQUAL
     return UNEQUAL
 
-test1 = ["A", "B", "C"]
-test2 = ["A", "B", "C"]
+test1 = ["A", "B", "C", "D"]
+test2 = ["A", "B"]
 
-#print(sublist(test1, test2))
-print(sublist([1, 2], [1, 2, 3]))
+print(sublist(test2, test1))
+#print(sublist([1, 2], [1, 2, 3]))
