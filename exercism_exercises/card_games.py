d5160b20ab831441e16b26f2cdc005d1bcd12384
Michal Moudrý <moudry.michal@live.com>
2022-10-24 15:39:11 +0200

    Created card games exercise module

---
+++ exercism_exercises/card_games.py
@@ -0,0 +1,71 @@
+"""
+Card games exercise module.
+"""
+
+def get_rounds(number) -> list[int]:
+    """Create a list containing the current and next two round numbers.
+
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
+    """
+    return list(range(number, number + 3))
+
+
+def concatenate_rounds(rounds_1: list[int], rounds_2: list[int]) -> list[int]:
+    """Concatenate two lists of round numbers.
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    """
+    rounds_1.extend(rounds_2)
+    return rounds_1
+
+
+def list_contains_round(rounds: list[int], number: int) -> bool:
+    """Check if the list of rounds contains the specified number.
+
+    :param rounds: list - rounds played.
+    :param number: int - round number.
+    :return: bool - was the round played?
+    """
+    return number in rounds
+
+
+def card_average(hand: list[int]) -> float:
+    """Calculate and returns the average card value from the list.
+
+    :param hand: list - cards in hand.
+    :return: float - average value of the cards in the hand.
+    """
+    return sum(hand) / len(hand)
+
+
+def approx_average_is_average(hand: list[int]):
+    """Return if an average is using (first + last index values ) OR ('middle' card) == calculated average.
+
+    :param hand: list - cards in hand.
+    :return: bool - does one of the approximate averages equal the `true average`?
+    """
+
+    pass
+
+
+def average_even_is_average_odd(hand):
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
+
+    :param hand: list - cards in hand.
+    :return: bool - are even and odd averages equal?
+    """
+
+    pass
+
+
+def maybe_double_last(hand):
+    """Multiply a Jack card value in the last index position by 2.
+
+    :param hand: list - cards in hand.
+    :return: list - hand with Jacks (if present) value doubled.
+    """
+
+    pass
