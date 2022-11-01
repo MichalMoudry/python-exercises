88ff8661f9da2e8e346e01a142b6adba463b42d6
Michal Moudrý <moudry.michal@live.com>
2022-11-01 16:13:52 +0100

    Updated pig latin exercise

--- exercism_exercises/pig_latin.py
+++ exercism_exercises/pig_latin.py
@@ -2,11 +2,34 @@
 Pig latin exercise module.
 """
 
-VOWELS = ["a", "e", "i", "o", "u"]
-CONSONANTS = ["b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p", "q", "s", "t", "v", "x", "z", "h", "r", "w", "y"]
-CONSONANT_CLUSTERS = []
+VOWELS = ("a", "e", "i", "o", "u", "xr", "yt")
+CONSONANTS = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z")
 
 def translate(text: str) -> str:
-    return text
+    lower_text = text.lower()
+    result: list[str] = []
+    consonants: list[str] = []
+    text_length = len(text)
+    first_letter = lower_text[0]
+    for i, char in enumerate(text):
+        if i < text_length - 1 and lower_text[i + 1] in CONSONANTS:
+            consonants.append(char)
+            consonants.append(text[i + 1])
+        else: break
+    if first_letter in VOWELS or lower_text[0:2] in VOWELS:
+        result.append(text)
+    """
+    if first_letter in VOWELS or first_two_letters in VOWELS:
+        result.append(text)
+    elif first_letter in CONSONANTS and second_and_third_letter != "qu":
+        result.append(text[1:])
+        result.append(text[0])
+    elif first_letter in CONSONANTS and second_and_third_letter == "qu":
+        result.append(text[3:])
+        result.append(text[0])
+        result.append(second_and_third_letter)
+    """
+    result.append("ay")
+    return "".join(result)
 
-print(translate("Pig"))
+print(translate("chair"))
