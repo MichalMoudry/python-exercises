4f74a217c53fa264d10d0927f3df118befb728f2
Michal Moudrý <moudry.michal@live.com>
2022-11-03 15:28:20 +0100

    Updated pig latin exercise

--- exercism_exercises/pig_latin.py
+++ exercism_exercises/pig_latin.py
@@ -2,7 +2,7 @@
 Pig latin exercise module.
 """
 
-VOWELS = ("a", "e", "i", "o", "u", "xr", "yt")
+VOWELS = ("a", "e", "i", "o", "u")
 CONSONANTS = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z")
 
 def translate(text: str) -> str:
@@ -11,13 +11,19 @@ def translate(text: str) -> str:
     consonants: list[str] = []
     text_length = len(text)
     first_letter = lower_text[0]
+    first_two_letters = lower_text[0:2]
     for i, char in enumerate(text):
         if i < text_length - 1 and lower_text[i + 1] in CONSONANTS:
             consonants.append(char)
             consonants.append(text[i + 1])
         else: break
-    if first_letter in VOWELS or lower_text[0:2] in VOWELS:
+    consonant_cluster = "".join(consonants)
+        
+    if first_letter in VOWELS or (first_two_letters == "xt" or first_two_letters == "yt"): # rule 1
         result.append(text)
+    elif first_letter in CONSONANTS: # rule 2
+        result.append(text[len(consonants):])
+        result.append(consonant_cluster)
     """
     if first_letter in VOWELS or first_two_letters in VOWELS:
         result.append(text)
