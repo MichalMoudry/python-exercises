1b3b2706217a73ce36921e19e6bb2f49928c96dc
Michal Moudrý <moudry.michal@live.com>
2022-11-04 16:20:29 +0100

    Updated pig latin exercise

--- exercism_exercises/pig_latin.py
+++ exercism_exercises/pig_latin.py
@@ -10,18 +10,9 @@ def translate(text: str) -> str:
     result: list[str] = []
     consonants: list[str] = []
     text_length = len(text)
-    first_letter = lower_text[0]
-    first_two_letters = lower_text[0:2]
-    for i, char in enumerate(text):
-        consonants.append(char)
-        if i < text_length - 1 and lower_text[i + 1] in CONSONANTS: continue
-        else: break
-    consonant_cluster = "".join(consonants)
-    cluster_length = len(consonant_cluster)
-    consonant_cluster_with_qu = text.find(consonant_cluster + "u")
-    consonant_cluster_with_y = text.find(consonant_cluster + "y")
-    
-    if first_letter in VOWELS or (first_two_letters == "xr" or first_two_letters == "yt"): # rule 1
+    # TODO: Find consonant cluster
+
+    """if first_letter in VOWELS or (first_two_letters == "xr" or first_two_letters == "yt"): # rule 1
         result.append(text)
     elif text.startswith(consonant_cluster) and consonant_cluster_with_qu == -1 and consonant_cluster_with_y == -1: # rule 2
         result.append(text[cluster_length:])
@@ -29,6 +20,10 @@ def translate(text: str) -> str:
     elif text.startswith(consonant_cluster) and consonant_cluster_with_qu != -1: # rule 3
         result.append(text[cluster_length + 1:])
         result.append(text[:cluster_length + 1])
+    elif consonant_cluster_with_y != -1: # rule 4
+        result.append(text[cluster_length:])
+        result.append(text[:cluster_length])
+        result.append("-")"""
     result.append("ay")
     return "".join(result)
 
