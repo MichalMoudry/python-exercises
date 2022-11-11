c6654491d79962621fc5a0dd32fb3fb89c2056bd
Michal Moudrý <moudry.michal@live.com>
2022-11-11 21:11:26 +0100

    Changes to sublist exercise

--- exercism_exercises/sublist.py
+++ exercism_exercises/sublist.py
@@ -8,31 +8,24 @@ SUPERLIST = "superlist"
 EQUAL = "equal"
 UNEQUAL = "unequal"
 
-def eval_superlist(list_one: list, list_two: list) -> str:
-    for item in enumerate(list_two):
-        if item[1] != list_one[item[0]]:
-            return UNEQUAL
-    return SUPERLIST
-
-def eval_sublist(list_one: list, list_two: list) -> str:
-    return SUBLIST
+def is_smaller_list_in_larger_list(smaller_list: list, larger_list: list) -> bool:
+    print(set(larger_list), set(smaller_list))
+    return True
 
 def sublist(list_one: list, list_two: list) -> str:
-    if list_one == list_two:
-        return EQUAL
-    try:
-        result = eval_superlist(list_one, list_two)
-        if result == SUPERLIST:
-            return SUPERLIST
-    except:
-        result = eval_sublist(list_one, list_two)
-        if result == SUBLIST:
-            return SUBLIST
-        return UNEQUAL
+    if list_one == list_two: return EQUAL
+    list_one_length = len(list_one)
+    list_two_length = len(list_two)
+    if list_one_length < list_two_length and is_smaller_list_in_larger_list(list_one, list_two):
+        return SUBLIST
+    elif list_one_length > list_two_length and is_smaller_list_in_larger_list(list_two, list_one):
+        return SUPERLIST
     return UNEQUAL
 
 test1 = ["A", "B", "C", "D"]
 test2 = ["A", "B"]
 
-print(sublist(test2, test1))
-#print(sublist([1, 2], [1, 2, 3]))
+"""print(sublist(test1, test2))
+print(sublist([1, 2], [1, 2, 3]))
+print(sublist([1, 3], [1, 2, 3]))"""
+print(sublist([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]))
