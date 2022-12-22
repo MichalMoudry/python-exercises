c0628f1977a1f8d75fd51153faa2e8c7353e8a1b
Michal Moudrý <moudry.michal@live.com>
2022-12-22 14:32:25 +0100

    Updated transpose

--- exercism_exercises/transpose.py
+++ exercism_exercises/transpose.py
@@ -3,4 +3,16 @@ Transpose exercise module.
 """
 
 def transpose(lines: list[str]):
-    pass
+    result: list[str] = []
+    for line in lines:
+        print([item for item in enumerate(line)])
+        for item in line:
+            result.append(item)
+    return result
+
+lines = ["ABC", "123"]
+#lines = ["Single line."]
+expected = ["A1", "B2", "C3"]
+result = transpose(lines)
+
+print(result, "=>", expected == result)
