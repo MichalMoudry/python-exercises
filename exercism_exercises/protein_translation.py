86b9395c5d3521223c29c05bf6fc8a0e5a9d54f4
Michal Moudrý <moudry.michal@live.com>
2023-01-06 21:15:16 +0100

    Updated protein translation

--- exercism_exercises/protein_translation.py
+++ exercism_exercises/protein_translation.py
@@ -2,6 +2,16 @@
 Protein translation exercise module.
 """
 
+PROTEINS = {
+    "Methionine": ("AUG",),
+    "Phenylalanine": ("UUU", "UUC"),
+    "Leucine": ("UUA", "UUG"),
+    "Serine": ("UCU", "UCC", "UCA", "UCG"),
+    "Tyrosine": ("UAU", "UAC"),
+    "Cysteine": ("UGU", "UGC"),
+    "Tryptophan": ("UGG",)
+}
+
 def chunk_iterable(iterable, chunk_size: int):
     """
     Function for chunking an iterable object.
@@ -13,8 +23,13 @@ def chunk_iterable(iterable, chunk_size: int):
     return result
 
 def proteins(strand: str):
-    print(chunk_iterable(strand, 3))
+    inpt = chunk_iterable(strand, 3)
+    print(inpt)
+    for protein in PROTEINS:
+        print("---", protein, PROTEINS[protein])
+        evl = [prot in inpt for prot in PROTEINS[protein]]
+        print(evl, all(evl))
     pass
 
 if __name__ == "__main__":
-    print(proteins("AUGUUUUCU"))
+    print(proteins("AUGUUUUCUUAAAUG"))
