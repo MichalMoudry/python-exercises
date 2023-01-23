c040c39491f59e5350dab12b60ec799ddca06879
Michal Moudrý <moudry.michal@live.com>
2023-01-23 17:52:17 +0100

    Updated matching brackets

--- exercism_exercises/matching_brackets.py
+++ exercism_exercises/matching_brackets.py
@@ -2,10 +2,25 @@
 Matching brackets exercise module.
 """
 
+PAIRS = {
+    "(": ")",
+    ")": "(",
+    "{": "}",
+    "[": "]"
+}
+
 def is_paired(input_string: str) -> bool:
-    unique_chars = set(input_string)
-    return True
+    unique_chars = list(set(input_string))
+    result = False
+    for char in unique_chars:
+        if char == "[":
+            print(char, PAIRS[char], PAIRS[char] in unique_chars)
+        elif char == "{":
+            print(char, PAIRS[char], PAIRS[char] in unique_chars)
+        elif char == "(" or char == ")":
+            print(char, PAIRS[char], PAIRS[char] in unique_chars)
+    return result
 
 if __name__ == "__main__":
-    print(is_paired("[[]]"), "Expected: True")
-    print(is_paired("[["), "Expected: False")
+    print(is_paired("([[]]{}"), "Expected: True")
+    print(is_paired("{[)][]}"), "Expected: False")
