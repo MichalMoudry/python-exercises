b478abec4f133bb05b72a0602e5dd78c16fbacf4
Michal Moudrý <moudry.michal@live.com>
2023-02-02 12:32:03 +0100

    Update matching_brackets.py

--- exercism_exercises/matching_brackets.py
+++ exercism_exercises/matching_brackets.py
@@ -12,24 +12,14 @@ PAIRS = {
 }
 
 def is_paired(input_string: str) -> bool:
-    unique_chars = set([char for char in input_string if char in PAIRS.keys()])
-    eval_result = False
-    for char in enumerate(unique_chars):
-        #eval_result = PAIRS[char] not in unique_chars
-        """if char == "[" and eval_result:
-            return False
-        elif char == "{" and eval_result:
-            return False
-        elif char == "(" and eval_result:
-            return False
-        elif char == "]" and eval_result:
-            return False
-        elif char == ")" and eval_result:
-            return False"""
-        print(f"{char} => {eval_result}")
-    return True
+    input_string = input_string.replace(" ", "")
+    print(input_string, "-"*20, sep="\n")
+    for char in input_string:
+        print(char)
+    return False
 
 if __name__ == "__main__":
     #print(is_paired("[[]]{}"), "Expected: True")
     #print(is_paired("{[)][]}"), "Expected: False")
-    print(is_paired("}{"), "Expected: False")
+    print(is_paired("{ { } }"))
+    #print(is_paired("}{"), "Expected: False")
